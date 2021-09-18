4dc3afcc684c7d07a93c85a5374765b9c960e7b3
Reno Bisschop <57941435+Reno2301@users.noreply.github.com>
2021-09-18 15:13:15 +0200

    Walking animation working

--- Game_Mechanics/Assets/Scripts/PlayerMovement.cs
+++ Game_Mechanics/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float moveSpeed = 5;
+
+    public Rigidbody2D playerRB;
+    public Animator animator;
+
+    Vector2 movement;
+
+    public void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
-    // Update is called once per frame
-    void Update()
+    //FixedUpdate for the physics.
+    void FixedUpdate()
     {
-        
+        playerRB.MovePosition(playerRB.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
