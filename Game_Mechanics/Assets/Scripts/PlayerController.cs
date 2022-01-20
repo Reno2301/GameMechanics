a403d2ec4ccf315b1f9c56c79a24718911147f2b
Reno Bisschop <reno.bisschop@icloud.com>
2022-01-20 16:27:00 +0100

    WalkingAnimation working

--- Game_Mechanics/Assets/Scripts/PlayerController.cs
+++ Game_Mechanics/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
 
     public LayerMask tileCollision;
 
+    public Animator animator;
+
     private void Start()
     {
         movePoint.parent = null;
@@ -18,21 +20,33 @@ public class PlayerController : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .1f, tileCollision))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .1f, tileCollision))
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
-            } else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .1f, tileCollision))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .1f, tileCollision))
                 {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
                 }
             }
         }
+
+        if (Input.GetKey(KeyCode.D))
+            animator.Play("Main_Player_Walk_Right");
+        else if (Input.GetKey(KeyCode.A))
+            animator.Play("Main_Player_Walk_Left");
+        else if (Input.GetKey(KeyCode.W))
+            animator.Play("Main_Player_Walk_Up");
+        else if (Input.GetKey(KeyCode.S))
+            animator.Play("Main_Player_Walk_Down");
+        else
+            animator.Play("Main_Player_Idle");
     }
 }
